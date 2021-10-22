22d44902e3a97faf2664976a1fbb43d1e46e9c6a
weelam <wl.lin@mail.utoronto.ca>
2021-10-22 18:37:22 -0400

    setted up react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import FindPage from "./pages/FindPage";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/find">
+            <FindPage />
+          </Route>
+          <Route path="/dashboard"></Route>
+          <Route path="/1"></Route>
+          <Route path="/2"></Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ frontend/src/pages/FindPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+    
+const FindPage = () => {
+  return (
+    <div>
+      <h1>Find</h1>
+    </div>
+  );
+};
+
+export default FindPage;
