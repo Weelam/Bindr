5430237ee58681a827daaef3d1ce7f10220d5362
weelam <wl.lin@mail.utoronto.ca>
2021-10-26 16:27:35 -0400

    created template for navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,15 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import FindPage from "./pages/FindPage";
 import Home from "./pages/Home";
-
+import Navbar from "./components/NavBar/Navbar";
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
           <Route exact path="/">
-            <Home />
+            <Navbar />
+            
           </Route>
           <Route path="/find">
             <FindPage />
