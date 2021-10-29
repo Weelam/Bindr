627a5f98aaeafc036e7524f76f90bb76cd587051
lupercaliaj <nuoyizi2333@gmail.com>
2021-10-29 09:34:43 -0400

    git

---
+++ frontend/src/components/Dashboard/UserStatus.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './UserCardStyle.css'
+const user = {
+    profileImg:
+        "https://images.mubicdn.net/images/avatars/108776/cache-108776-1523899185/images-large.png",
+    username: "Howard",
+    courses: ["CSC309", "CSC373", "CSC369"],
+    program: "Computer Science",
+    year: "Third",
+    bio: "Eiusmod qui mollit cillum dolore. Excepteur ut laborum veniam eu. Nulla nostrud officia mollit non elit magna qui adipi"
+}
+const UserStatus = () => {
+    return (
+        <div id="userstatus">
+            
+        </div>
+    )
+}
+
+export default UserStatus
--- frontend/src/components/NavBar/Navbar.js
+++ frontend/src/components/NavBar/Navbar.js
@@ -16,6 +16,12 @@ function Navbar({ auth }) {
             <Link className="link" to="find">
               Find
             </Link>
+
+            <Link className="link" to="dashboard">
+              Dashboard
+            </Link>
+
+
             <Link className="link" to="review">
               Reviews
             </Link>
