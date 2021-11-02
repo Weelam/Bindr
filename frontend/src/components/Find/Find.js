03d08d5b5dc8f702c14fe4b3bd25cfc989747d39
weelam <wl.lin@mail.utoronto.ca>
2021-11-02 17:28:44 -0400

    added animation to grid

--- frontend/src/components/Find/Find.js
+++ frontend/src/components/Find/Find.js
@@ -1,12 +1,57 @@
-import React from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import Grid from "@mui/material/Grid";
 import FindItem from "./FindItem";
+import CircularProgress from "@mui/material/CircularProgress";
+import Box from "@mui/material/Box";
 
 const Find = ({ users }) => {
+  const obs = useRef();
+  const [displayedUsers, setDisplayedUsers] = useState([]);
+  const [displayPointer, setDisplayPointer] = useState({ start: 0, end: 1 });
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(true);
+    handleDisplayedUsers();
+    console.log(displayPointer["start"], displayPointer["end"]);
+  }, [displayPointer]);
+
+  const handleDisplayedUsers = () => {
+    setDisplayedUsers((prev) => {
+      return [
+        ...prev,
+        ...users.slice(displayPointer["start"], displayPointer["end"]),
+      ];
+    });
+  };
+
+  const lastUserRef = useCallback((node) => {
+    if (obs.current) {
+      obs.current.disconnect();
+    }
+    obs.current = new IntersectionObserver((x) => {
+      if (x[0].isIntersecting) {
+        console.log("visible");
+        setDisplayPointer((prev) => {
+          return { start: prev["end"], end: prev["end"] + 10 };
+        });
+        setLoading(false);
+      }
+    });
+    if (node) obs.current.observe(node);
+  }, []);
+
   return (
     <div>
-      <Grid container columns={5}  rowSpacing={2} columnSpacing={2}>
-        {users.map((item, index) => {
+      <Grid container columns={5} rowSpacing={2} columnSpacing={2}>
+        {displayedUsers.map((item, index) => {
+          if (index === displayedUsers.length - 1) {
+            return (
+              <Grid ref={lastUserRef} item xs={1} key={index}>
+                <FindItem user={item} />
+              </Grid>
+            );
+          }
           return (
             <Grid item xs={1} key={index}>
               <FindItem user={item} />
@@ -14,6 +59,7 @@ const Find = ({ users }) => {
           );
         })}
       </Grid>
+      {loading && <CircularProgress style={{ height: "100%" }} />}
     </div>
   );
 };
